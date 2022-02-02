8965f938e07473aab9b96fb98e89ce3d4a4cf82a
Drako <abhishek491981@gmail.com>
2022-02-02 17:46:08 +0530

    appbar and nav bar is ready

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:flutter_feather_icons/flutter_feather_icons.dart";
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +9,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int index = 0, activeTab = 0;
+  List navIcon = [
+    FeatherIcons.home,
+    FeatherIcons.search,
+    FeatherIcons.book,
+    FeatherIcons.settings,
+  ];
+
+  Widget getNav() {
+    return Container(
+      // alignment: BottomAppBar,
+      height: 40,
+      decoration: const BoxDecoration(
+        color: Colors.black,
+      ),
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(navIcon.length, (index) {
+            return IconButton(
+              icon: Icon(
+                navIcon[index],
+                color:
+                    activeTab == index ? const Color(0xFF1ed661) : Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  activeTab = index;
+                });
+              },
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +63,19 @@ class _HomeState extends State<Home> {
             left: 2,
             right: 0,
           ),
-          child: Icon(Icons.music_note),
+          child: Icon(
+            Icons.music_note,
+            color: Color(0xFF1ed661),
+          ),
         ),
         title: const Padding(
           padding: EdgeInsets.only(
             left: 0,
           ),
+          // child: Image(
+          // height: 500,
+          // image: AssetImage('assets/images/spotify.png'),
+          // ),
           child: Text(
             'Spotify',
             style: TextStyle(
@@ -35,6 +86,7 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
+      bottomNavigationBar: getNav(),
     );
   }
 }
