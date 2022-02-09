765165194e24f857a01310ecb6c26d2cc42f6883
Drako <abhishek491981@gmail.com>
2022-02-09 20:59:32 +0530

    open album page added

--- lib/album.dart
+++ lib/album.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+var primary = const Color(0xFF1ed661);
 
 class AlbumPage extends StatefulWidget {
   final dynamic song;
 
   const AlbumPage({Key? key, this.song}) : super(key: key);
-
 
   @override
   State<AlbumPage> createState() => _AlbumPageState();
@@ -13,8 +15,104 @@ class AlbumPage extends StatefulWidget {
 class _AlbumPageState extends State<AlbumPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.amber,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: getAppBody(),
+    );
+  }
+
+  Widget getAppBody() {
+    var size = MediaQuery.of(context).size;
+    var heightForAlbum = MediaQuery.of(context).size.height;
+
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                width: size.width,
+                height: heightForAlbum / 3.5,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      widget.song['img'],
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.song['title'],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 12,
+                        ),
+                        child: Text(
+                          'Subscribe',
+                          style: TextStyle(
+                            letterSpacing: 2,
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              )
+            ],
+          ),
+          // we can use safearea widget for row to place all the content in correct area
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FeatherIcons.moreVertical,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
